5f3add89da6cdcc1e7133102aa6b6eeb441ef02c
Manuel Cañete <mcanete@plainconcepts.com>
2019-05-14 18:54:54 +0200

    Add grafana database with MySQL as container

--- deploy/infrastructure/aci.tf
+++ deploy/infrastructure/aci.tf
@@ -16,6 +16,26 @@ resource "azurerm_container_group" "containergroup" {
       port     = 3000
       protocol = "TCP"
     }
+
+    environment_variables {
+      GF_DATABASE_TYPE      = "mysql"
+      GF_DATABASE_HOST      = "127.0.0.1:3306"
+      GF_DATABASE_NAME      = "grafana"
+      GF_DATABASE_USER      = "root"
+      GF_DATABASE_PASSWORD  = "P2__w0rd"
+    }
+  }
+
+  container {
+    name   = "db"
+    image  = "mysql:5.7"
+    cpu    = "0.5"
+    memory = "1"
+
+    environment_variables {
+      MYSQL_DATABASE      = "grafana"
+      MYSQL_ROOT_PASSWORD = "P2__w0rd"
+    }
   }
 
   tags = "${local.common_tags}"
